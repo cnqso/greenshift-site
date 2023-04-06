a6995757688de6a1f559d6fd97055443d03bad2e
unknown <wmkelly@umich.edu>
2023-04-06 00:26:33 -0400

    Big backend moves

--- src/RichText.tsx
+++ src/RichText.tsx
@@ -1,19 +1,16 @@
+/** @format */
+
 import React, { useState, useEffect, useRef } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 
+
 const modules = {
-	toolbar: [[{ header: "1" },
-    { header: "2" }],
-		[
-			
-			"bold",
-			"italic",
-			"underline"],[
-			{ list: "ordered" },
-			{ list: "bullet" },
-		],
+	toolbar: [
+		[{ header: "1" }, { header: "2" }],
+		["bold", "italic", "underline"],
+		[{ list: "ordered" }, { list: "bullet" }],
 	],
 	clipboard: {
 		// toggle to add extra line breaks when pasting HTML:
@@ -25,6 +22,7 @@ const formats = ["header", "font", "size", "bold", "italic", "underline", "list"
 export default function App() {
 	const [HTMLText, setHTMLText] = useState("Write something");
 	const [plainText, setPlainText] = useState("Write something");
+	const [responseText, setResponseText] = useState("No Response Yet");
 	const quillRef = useRef(null);
 
 	function handleChange(content, delta, source, editor) {
@@ -32,7 +30,32 @@ export default function App() {
 		setPlainText(editor.getText());
 	}
 
-    // For later integration with the backend
+	const sendToCluod = async (inputText: string) => {
+		const response = await fetch('http://localhost:8000/api/reverse/', {
+		  method: 'POST',
+		  headers: {
+			'Content-Type': 'application/json',
+		  },
+		  body: JSON.stringify({ text: inputText }),
+		});
+	
+		if (response.ok) {
+		  const data = await response.json();
+		  setResponseText(data.reversed_text);
+		  console.log(JSON.stringify(data, null, 2))
+		} else {
+		  console.error('An error occurred while fetching the reversed text.');
+		}
+	  };
+
+
+	function submit(e: any) {
+		e.preventDefault();
+		console.log(plainText);
+		sendToCluod(plainText);
+	}
+
+	// For later integration with the backend
 
 	// function highlightLongSentences(quill) {
 	// 	const text = quill.getText();
@@ -43,9 +66,9 @@ export default function App() {
 	// 		if (sentence.length > 30) {
 	// 			quill.formatText(index, sentence.length+1, "bold", true);
 	// 		}
-    //         else {
-    //             quill.formatText(index, sentence.length+1, "bold", false);
-    //         }
+	//         else {
+	//             quill.formatText(index, sentence.length+1, "bold", false);
+	//         }
 	// 		index += sentence.length+1;
 	// 	}
 	// }
@@ -58,14 +81,21 @@ export default function App() {
 	// }, [plainText]);
 
 	return (
+        <>
 		<ReactQuill
 			ref={quillRef}
-			theme="snow"
+			theme='snow'
 			value={HTMLText}
 			onChange={handleChange}
 			modules={modules}
 			formats={formats}
 			bounds={".RichText"}
 		/>
+        <br/>
+        <button onClick={submit}>Submit</button>
+		<br/>
+		{responseText}
+        </>
+
 	);
 }
