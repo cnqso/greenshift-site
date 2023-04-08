5d6bb89d3f6cb93a6ca872a001941f9be7a48fad
unknown <wmkelly@umich.edu>
2023-04-08 17:47:58 -0400

    Analysis moved to text box

--- src/RichText.tsx
+++ src/RichText.tsx
@@ -4,55 +4,106 @@ import React, { useState, useEffect, useRef } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-
-
 const modules = {
 	toolbar: [
-		[{ header: "1" }, { header: "2" }],
-		["bold", "italic", "underline"],
-		[{ list: "ordered" }, { list: "bullet" }],
+		["clean"],
 	],
 	clipboard: {
 		// toggle to add extra line breaks when pasting HTML:
 		matchVisual: false,
 	},
 };
-const formats = ["header", "font", "size", "bold", "italic", "underline", "list", "bullet"];
+const formats = ["header", "font", "size", "bold", "italic", "underline", "list", "bullet", "background"];
+
+
+function htmlToPlainText(html: string): string {
+	// Create a DOMParser to parse the input HTML
+	const parser = new DOMParser();
+	const doc = parser.parseFromString(html, "text/html");
+	// Replace the desired tags with their respective markdown equivalents
+	function replaceTags(element: Element): void {
+	  Array.from(element.children).forEach((child) => {
+		if (child.tagName.toLowerCase() === "p") {
+		  child.textContent = `${child.textContent}\n`;
+		}
+		replaceTags(child);
+	  });
+	}
+	replaceTags(doc.body);
+	// Recursively traverse the DOM tree and create a plain text string
+	function getText(node: Node): string {
+	  if (node.nodeType === Node.TEXT_NODE) {
+		return node.textContent || "";
+	  }
+	  let text = "";
+	  Array.from(node.childNodes).forEach((child) => {
+		text += getText(child);
+	  });
+	  return text;
+	}
+	return getText(doc.body);
+  }
 
 export default function App() {
 	const [HTMLText, setHTMLText] = useState("Write something");
 	const [plainText, setPlainText] = useState("Write something");
-	const [responseText, setResponseText] = useState("No Response Yet");
+	const [responseText, setResponseText] = useState("");
 	const quillRef = useRef(null);
 
 	function handleChange(content, delta, source, editor) {
 		setHTMLText(content);
+		const newText = htmlToPlainText(content);
+		setPlainText(newText);
 		setPlainText(editor.getText());
 	}
 
-	const sendToCluod = async (inputText: string) => {
-		const response = await fetch('http://localhost:8000/api/reverse/', {
-		  method: 'POST',
-		  headers: {
-			'Content-Type': 'application/json',
-		  },
-		  body: JSON.stringify({ text: inputText }),
+	const sendToCluodAnalyze = async (inputText: string) => {
+		const response = await fetch("http://localhost:8000/api/reverse/", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ text: inputText }),
 		});
-	
+
 		if (response.ok) {
-		  const data = await response.json();
-		  setResponseText(data.reversed_text);
-		  console.log(JSON.stringify(data, null, 2))
+			const data = await response.json();
+			setHTMLText(data.analyzed_text)
+			console.log(JSON.stringify(data, null, 2));
 		} else {
-		  console.error('An error occurred while fetching the reversed text.');
+			console.error("An error occurred while fetching the reversed text.");
 		}
-	  };
+	};
 
+	const sendToCluodGPT = async (inputText: string) => {
+		const response = await fetch("http://localhost:8000/api/readability/", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ user_token: "WilliamKelly", desired_reading_level: 5, texts: [inputText] }),
+		});
 
-	function submit(e: any) {
+		if (response.ok) {
+			const data = await response.json();
+			console.log(JSON.stringify(data.readability_data[0].output, null, 2));
+			setResponseText(`<div>${data.readability_data[0].output}</div>`);
+			
+		} else {
+			console.error("An error occurred while fetching the reversed text.");
+		}
+	};
+
+	function submitAnalyze(e: any) {
 		e.preventDefault();
 		console.log(plainText);
-		sendToCluod(plainText);
+		sendToCluodAnalyze(plainText);
+	}
+
+	function submitGPT(e: any) {
+		e.preventDefault();
+		console.log(plainText);
+		sendToCluodGPT(plainText);
 	}
 
 	// For later integration with the backend
@@ -80,25 +131,28 @@ export default function App() {
 	// 	}
 	// }, [plainText]);
 
-
 	return (
-        <>
-		<ReactQuill
-			ref={quillRef}
-			theme='snow'
-			value={HTMLText}
-			onChange={handleChange}
-			modules={modules}
-			formats={formats}
-			bounds={".RichText"}
-		/>
-        <br/>
-        <button onClick={submit}>Submit</button>
-		<br/>
-		<div className="responseBox">
-		<div dangerouslySetInnerHTML={{ __html: responseText}}/>
-		</div>
-        </>
-
+		<>
+			<ReactQuill
+				ref={quillRef}
+				theme='snow'
+				value={HTMLText}
+				onChange={handleChange}
+				modules={modules}
+				formats={formats}
+				bounds={".RichText"}
+			/>
+			<br />
+			<button style={{marginRight: "10px"}} onClick={submitAnalyze}>Analyze</button>
+			<button style={{marginLeft: "10px"}}onClick={submitGPT}>Simplify</button>
+			<br />
+			<br />
+			<div className='responseBox'>
+				<div dangerouslySetInnerHTML={{ __html: responseText }} />
+			</div>
+			<div className='responseBox'>
+				<div>{HTMLText}</div>
+			</div>
+		</>
 	);
 }
