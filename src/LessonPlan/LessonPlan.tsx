4652fe1a3170fe2841dea68996d150d67d982d75
unknown <wmkelly@umich.edu>
2023-05-07 19:15:35 -0400

    total rewrite of the lesson plan stepper structure and its backend integration

--- src/LessonPlan/LessonPlan.tsx
+++ src/LessonPlan/LessonPlan.tsx
@@ -1,91 +1,213 @@
-import React, { useState } from "react";
-import LessonStep from "./LessonStep";
-import "./LessonStep.css";
-import NeuralNetworkGen from "../assets/NeuralNetworkGen";
+/** @format */
+
+import React, { useState, ReactNode } from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { StepOne, StepTwo, StepThree, StepFour, StepFive } from "./LessonStep";
 import { sendToCluod } from "../requests";
+import NeuralNetworkGen from "../assets/NeuralNetworkGen";
 
+const steps = ["Objectives", "Assessments", "Activities", "Materials", "Review"];
 
-interface LessonPlanRequest {
-  subject: string;
-  topic: string;
-  gradeLevel: number;
-  stateStandards: string | null;
-  focusPoints: string | null;
-  priorKnowledge: string | null;
-  scaffoldingGoals: string | null;
-  targetSkills: string | null;
+function StepContent({
+	step,
+	handleNext,
+	handleSkip,
+	handleBack,
+	outputText,
+	setOutputText,
+}: {
+	step: number;
+	handleNext: any;
+	handleSkip: any;
+	handleBack: any;
+	outputText: string;
+	setOutputText: any;
+}) {
+	switch (step) {
+		case 0:
+			return <StepOne handleNext={handleNext} />;
+		case 1:
+			return (
+				<StepTwo
+					handleNext={handleNext}
+					handleBack={handleBack}
+					outputText={outputText}
+					setOutputText={setOutputText}
+				/>
+			);
+		case 2:
+			return (
+				<StepThree
+					handleNext={handleNext}
+					handleBack={handleBack}
+					outputText={outputText}
+					setOutputText={setOutputText}
+				/>
+			);
+		case 3:
+			return (
+				<StepFour
+					handleNext={handleNext}
+					handleSkip={handleSkip}
+					handleBack={handleBack}
+					outputText={outputText}
+					setOutputText={setOutputText}
+				/>
+			);
+		case 4:
+			return (
+				<StepFive
+					handleNext={handleNext}
+					handleBack={handleBack}
+					outputText={outputText}
+					setOutputText={setOutputText}
+				/>
+			);
+		default:
+			return <div />;
+	}
 }
 
+interface LessonPlanRequest {
+	subject: string;
+	topic: string;
+	gradeLevel: number;
+	stateStandards: string | null;
+	focusPoints: string | null;
+	priorKnowledge: string | null;
+	scaffoldingGoals: string | null;
+	targetSkills: string | null;
+}
 
+// Form in StepContent in submitted to backend
+// Show loading screen while waiting for backend
+// Receive response from backend and display on the next step
 
+//submitToBackend() function which returns either the display information or false
+// submitToBackend() is placed in handleNext
+// handleNext takes in the form information
 
+// handleNext() then passes the form information to a state variable
 
+export default function HorizontalLinearStepper() {
+	const [activeStep, setActiveStep] = useState(0);
+	const [skipped, setSkipped] = useState(new Set<number>());
+	const [outputText, setOutputText] = useState<string>("");
+	const [loading, setLoading] = useState<boolean>(false);
 
-//Between each step, add AI generation loading screen that ends when you've received the generations.
-// Create automatic formatting, so that GPT can simply respond in plain text or numbered lists, and the formatting will be done automatically.
-// Can be done in HTML just fine and then converted to PDF, though DOCX will be more difficult.
+	const submitStep = async (input: any) => {
+		setLoading(true);
 
-const LessonPlan = () => {
-  const [currentStep, setCurrentStep] = useState(1);
-  const [loading, setLoading] = useState(false);
+		const body = { step: activeStep, input: input };
+		const data = await sendToCluod("lessonplan", body);
+		if (data) {
+			console.log(data);
+		} else {
+			console.error("An error occurred while fetching the simplified text.");
+			return false;
+		}
+		setLoading(false);
+		return data;
+	};
 
+	const isStepOptional = (step: number) => {
+		return step === 3;
+	};
 
-  async function nextStep(input: any) {
-    const output = await sendStep(currentStep, input);
+	const isStepSkipped = (step: number) => {
+		return skipped.has(step);
+	};
 
-    if (output) {
-    setCurrentStep(currentStep + 1);
-    } else {
-      console.error("An error occurred while fetching the simplified text.");
-    }
-  };
+	const handleNext = async (data: any) => {
+		let newSkipped = skipped;
+		if (isStepSkipped(activeStep)) {
+			newSkipped = new Set(newSkipped.values());
+			newSkipped.delete(activeStep);
+		}
 
-  const prevStep = () => {
-    setCurrentStep(currentStep - 1);
-  };
+		const serverOutput = await submitStep(data);
+		if (serverOutput) {
+			setOutputText(serverOutput.lessonPlan);
+			setActiveStep((prevActiveStep) => prevActiveStep + 1);
+		} else {
+			console.error("An error occurred while fetching the simplified text.");
+		}
 
-  const sendStep = async (step: number, input: any) => {
-    setLoading(true);
-    
-      const body = { step: currentStep, input: input};
-      const data = await sendToCluod("lessonplan", body);
-      if (data) {
-        console.log(data);
-      } else {
-        console.error("An error occurred while fetching the simplified text.");
-        return false;
-      }
-      setLoading(false);
-      return true;
-    }
+		setSkipped(newSkipped);
+	};
 
-  return (
-    <div className="lesson-plan-generator">
-      <h1>Lesson Plan Generator</h1>
-      <div className="progress-bar">
-        <div className="progress" style={{ width: `${(currentStep - 1) * 20}%` }}></div>
-      </div>
-      <LessonStep step={currentStep} nextStep={nextStep} prevStep={prevStep} />
-      {loading ? (<NeuralNetworkGen/>):
-      <div className="step-navigation">
-        {currentStep > 1 && (
-          <button className="prev-button" onClick={prevStep}>
-            Previous Step
-          </button>
-        )}
-        {currentStep < 5 && (
-          <button className="next-button" onClick={nextStep}>
-            Next Step
-          </button>
-        )}
-        {currentStep === 5 && (
-          <button className="finish-button" onClick={() => alert("Lesson Plan Created!")}>
-            Finish
-          </button>
-        )}
-      </div>}
-    </div>
-  );
-};
+	const handleBack = () => {
+		setActiveStep((prevActiveStep) => prevActiveStep - 1);
+	};
 
-export default LessonPlan;
+	const handleSkip = () => {
+		if (!isStepOptional(activeStep)) {
+			// You probably want to guard against something like this,
+			// it should never occur unless someone's actively trying to break something.
+			throw new Error("You can't skip a step that isn't optional.");
+		}
+
+		setActiveStep((prevActiveStep) => prevActiveStep + 1);
+		setSkipped((prevSkipped) => {
+			const newSkipped = new Set(prevSkipped.values());
+			newSkipped.add(activeStep);
+			return newSkipped;
+		});
+	};
+
+	const handleReset = () => {
+		setActiveStep(0);
+	};
+
+	return (
+		<Box sx={{ width: "100%" }}>
+			<Stepper activeStep={activeStep}>
+				{steps.map((label, index) => {
+					const stepProps: { completed?: boolean } = {};
+					const labelProps: {
+						optional?: ReactNode;
+					} = {};
+					if (isStepOptional(index)) {
+						labelProps.optional = <Typography variant='caption'>Optional</Typography>;
+					}
+					if (isStepSkipped(index)) {
+						stepProps.completed = false;
+					}
+					return (
+						<Step key={label} {...stepProps}>
+							<StepLabel {...labelProps}>{label}</StepLabel>
+						</Step>
+					);
+				})}
+			</Stepper>
+
+			{activeStep === steps.length ? (
+				<div>
+					<Typography sx={{ mt: 2, mb: 1 }}>All steps completed - you&apos;re finished</Typography>
+					<Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+						<Box sx={{ flex: "1 1 auto" }} />
+						<Button onClick={handleReset}>Reset</Button>
+					</Box>
+				</div>
+			) : loading ? (
+				<NeuralNetworkGen />
+			) : (
+				<>
+					<StepContent
+						step={activeStep}
+						handleNext={handleNext}
+						handleSkip={handleSkip}
+						handleBack={handleBack}
+						outputText={outputText}
+						setOutputText={setOutputText}
+					/>
+				</>
+			)}
+		</Box>
+	);
+}
--- src/LessonPlan/LessonStep.tsx
+++ src/LessonPlan/LessonStep.tsx
@@ -4,9 +4,12 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-
-
-
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const schema = yup.object().shape({
 	subject: yup.string().required("Subject is required."),
@@ -21,32 +24,7 @@ const schema = yup.object().shape({
 
 type FormData = yup.InferType<typeof schema>;
 
-export default function LessonStep({
-	step,
-	nextStep,
-	prevStep,
-}: {
-	step: number;
-	nextStep: any;
-	prevStep: any;
-}) {
-	switch (step) {
-		case 1:
-			return <StepOne nextStep={nextStep} />;
-		case 2:
-			return <StepTwo nextStep={nextStep} prevStep={prevStep} />;
-		case 3:
-			return <StepThree nextStep={nextStep} prevStep={prevStep} />;
-		case 4:
-			return <StepFour nextStep={nextStep} prevStep={prevStep} />;
-		case 5:
-			return <StepFive nextStep={nextStep} prevStep={prevStep} />;
-		default:
-			return <StepOne nextStep={nextStep} />;
-	}
-}
-
-const StepOne = ({ nextStep }: { nextStep: any }) => {
+const StepOne = ({ handleNext }: { handleNext: any }) => {
 	const {
 		register,
 		handleSubmit,
@@ -56,138 +34,254 @@ const StepOne = ({ nextStep }: { nextStep: any }) => {
 	});
 
 	const onSubmit = (data: FormData) => {
-		nextStep(data);
+		console.log(JSON.stringify(data, null, 2));
+		handleNext(data);
 	};
 
 	return (
-		<div className='container'>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<div className='form-group'>
-					<label htmlFor='subject' className='label'>
-						Subject* :
-					</label>
-					<input
-						id='subject'
-						{...register("subject")}
-						className={`input ${errors.subject ? "error" : ""}`}
-					/>
-					<p className='error-message'>{errors.subject?.message}</p>
+		<div>
+			<div className='container'>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className='form-group'>
+						<label htmlFor='subject' className='label'>
+							Subject* :
+						</label>
+						<input
+							id='subject'
+							{...register("subject")}
+							className={`input ${errors.subject ? "error" : ""}`}
+						/>
+						<p className='error-message'>{errors.subject?.message}</p>
 
-					<label htmlFor='topic' className='label'>
-						Topic* :
-					</label>
-					<input
-						id='topic'
-						{...register("topic")}
-						className={`input ${errors.topic ? "error" : ""}`}
-					/>
-					<p className='error-message'>{errors.topic?.message}</p>
+						<label htmlFor='topic' className='label'>
+							Topic* :
+						</label>
+						<input
+							id='topic'
+							{...register("topic")}
+							className={`input ${errors.topic ? "error" : ""}`}
+						/>
+						<p className='error-message'>{errors.topic?.message}</p>
 
-					<label htmlFor='grade' className='label'>
-						Grade Level* :
-					</label>
-					<input
-						id='grade'
-						{...register("gradeLevel")}
-						className={`input ${errors.gradeLevel ? "error" : ""}`}
-					/>
-					<p className='error-message'>{errors.gradeLevel?.message}</p>
+						<label htmlFor='grade' className='label'>
+							Grade Level* :
+						</label>
+						<input
+							id='grade'
+							{...register("gradeLevel")}
+							className={`input ${errors.gradeLevel ? "error" : ""}`}
+						/>
+						<p className='error-message'>{errors.gradeLevel?.message}</p>
 
-					<label htmlFor='stateStandards' className='label'>
-						State Standards:
-					</label>
-					<input
-						id='stateStandards'
-						{...register("stateStandards")}
-						className={`input ${errors.stateStandards ? "error" : ""}`}
-					/>
-					<p className='error-message'>{errors.stateStandards?.message}</p>
+						<label htmlFor='stateStandards' className='label'>
+							State Standards:
+						</label>
+						<input
+							id='stateStandards'
+							{...register("stateStandards")}
+							className={`input ${errors.stateStandards ? "error" : ""}`}
+						/>
+						<p className='error-message'>{errors.stateStandards?.message}</p>
 
-					<label htmlFor='focusPoints' className='label'>
-						Focus Points:
-					</label>
-					<input
-						id='focusPoints'
-						{...register("focusPoints")}
-						className={`input ${errors.focusPoints ? "error" : ""}`}
-					/>
-					<p className='error-message'>{errors.focusPoints?.message}</p>
+						<label htmlFor='focusPoints' className='label'>
+							Focus Points:
+						</label>
+						<input
+							id='focusPoints'
+							{...register("focusPoints")}
+							className={`input ${errors.focusPoints ? "error" : ""}`}
+						/>
+						<p className='error-message'>{errors.focusPoints?.message}</p>
 
-					<label htmlFor='priorKnowledge' className='label'>
-						Prior Knowledge:
-					</label>
-					<input
-						id='priorKnowledge'
-						{...register("priorKnowledge")}
-						className={`input ${errors.priorKnowledge ? "error" : ""}`}
-					/>
-					<p className='error-message'>{errors.priorKnowledge?.message}</p>
+						<label htmlFor='priorKnowledge' className='label'>
+							Prior Knowledge:
+						</label>
+						<input
+							id='priorKnowledge'
+							{...register("priorKnowledge")}
+							className={`input ${errors.priorKnowledge ? "error" : ""}`}
+						/>
+						<p className='error-message'>{errors.priorKnowledge?.message}</p>
 
-					<label htmlFor='scaffoldingGoals' className='label'>
-						Scaffolding Goals:
-					</label>
-					<input
-						id='scaffoldingGoals'
-						{...register("scaffoldingGoals")}
-						className={`input ${errors.scaffoldingGoals ? "error" : ""}`}
-					/>
-					<p className='error-message'>{errors.scaffoldingGoals?.message}</p>
+						<label htmlFor='scaffoldingGoals' className='label'>
+							Scaffolding Goals:
+						</label>
+						<input
+							id='scaffoldingGoals'
+							{...register("scaffoldingGoals")}
+							className={`input ${errors.scaffoldingGoals ? "error" : ""}`}
+						/>
+						<p className='error-message'>{errors.scaffoldingGoals?.message}</p>
 
-					<label htmlFor='targetSkills' className='label'>
-						Target Skills:
-					</label>
-					<input
-						id='targetSkills'
-						{...register("targetSkills")}
-						className={`input ${errors.targetSkills ? "error" : ""}`}
-					/>
-					<p className='error-message'>{errors.targetSkills?.message}</p>
+						<label htmlFor='targetSkills' className='label'>
+							Target Skills:
+						</label>
+						<input
+							id='targetSkills'
+							{...register("targetSkills")}
+							className={`input ${errors.targetSkills ? "error" : ""}`}
+						/>
+						<p className='error-message'>{errors.targetSkills?.message}</p>
+					</div>
+					<Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+						<Button color='inherit' disabled={true} sx={{ mr: 1 }}>
+							Back
+						</Button>
+						<Box sx={{ flex: "1 1 auto" }} />
+
+						<Button type='submit'>{"Next"}</Button>
+					</Box>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+const StepTwo = ({
+	handleNext,
+	handleBack,
+	outputText,
+	setOutputText,
+}: {
+	handleNext: any;
+	handleBack: any;
+	outputText: string;
+	setOutputText: any;
+}) => {
+	const onSubmit = () => {
+		console.log(JSON.stringify({ outputText }, null, 2));
+		handleNext({ outputText });
+	};
+
+	return (
+		<div>
+			<div className='step-one'>
+				<h2>Step 2: Review Objectives</h2>
+				{outputText}
+				<div>Retrieve the generated lesson objectives, make any desired edits</div>
+			</div>
+			<Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+				<Button color='inherit' onClick={handleBack} sx={{ mr: 1 }}>
+					Back
+				</Button>
+				<Box sx={{ flex: "1 1 auto" }} />
+
+				<Button onClick={onSubmit}>{"Next"}</Button>
+			</Box>
+		</div>
+	);
+};
+
+const StepThree = ({
+	handleNext,
+	handleBack,
+	outputText,
+	setOutputText,
+}: {
+	handleNext: any;
+	handleBack: any;
+	outputText: string;
+	setOutputText: any;
+}) => {
+	const onSubmit = () => {
+		console.log(JSON.stringify({ outputText }, null, 2));
+		handleNext({ outputText });
+	};
+	return (
+		<div>
+			<div className='step-one'>
+				<h2>Step 3: Review Assessments</h2>
+				{outputText}
+				<div>
+					Retrieve the generated assessments for the lesson objectives, make any desired edits
 				</div>
-				{/* Other form elements */}
-				<button type='submit'>Submit</button>
-			</form>
-		</div>
-	);
-};
-
-const StepTwo = ({ nextStep, prevStep }: { nextStep: any; prevStep: any }) => {
-	return (
-		<div className='step-one'>
-			<h2>Step 2: Review Objectives</h2>
-			<div>Retrieve the generated lesson objectives, make any desired edits</div>
-		</div>
-	);
-};
-
-const StepThree = ({ nextStep, prevStep }: { nextStep: any; prevStep: any }) => {
-	return (
-		<div className='step-one'>
-			<h2>Step 3: Review Assessments</h2>
-			<div>Retrieve the generated assessments for the lesson objectives, make any desired edits</div>
-		</div>
-	);
-};
-const StepFour = ({ nextStep, prevStep }: { nextStep: any; prevStep: any }) => {
-	return (
-		<div className='step-one'>
-			<h2>Step 4: Review Activities</h2>
-			<div>
-				Review the generated activities, make any desired edits, then either download as PDF/docx or
-				continue to step 5: generate materials
 			</div>
+			<Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+				<Button color='inherit' onClick={handleBack} sx={{ mr: 1 }}>
+					Back
+				</Button>
+				<Box sx={{ flex: "1 1 auto" }} />
+
+				<Button onClick={onSubmit}>{"Next"}</Button>
+			</Box>
 		</div>
 	);
 };
-const StepFive = ({ nextStep, prevStep }: { nextStep: any; prevStep: any }) => {
+const StepFour = ({
+	handleNext,
+	handleSkip,
+	handleBack,
+	outputText,
+	setOutputText,
+}: {
+	handleNext: any;
+	handleSkip: any;
+	handleBack: any;
+	outputText: string;
+	setOutputText: any;
+}) => {
+	const onSubmit = () => {
+		console.log(JSON.stringify({ outputText }, null, 2));
+		handleNext({ outputText });
+	};
 	return (
-		<div className='step-one'>
-			<h2>Step 5: Review Materials</h2>
-			<div>
-				Review the generated materials, make any desired edits, then either download as PDF/docx or
-				continue to step 5: generate materials
+		<div>
+			<div className='step-one'>
+				<h2>Step 4: Review Activities</h2>
+				{outputText}
+				<div>
+					Review the generated activities, make any desired edits, then either download as PDF/docx
+					or continue to step 5: generate materials
+				</div>
 			</div>
+			<Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+				<Button color='inherit' onClick={handleBack} sx={{ mr: 1 }}>
+					Back
+				</Button>
+				<Box sx={{ flex: "1 1 auto" }} />
+				<Button color='inherit' onClick={handleSkip} sx={{ mr: 1 }}>
+					Skip
+				</Button>
+				<Button onClick={onSubmit}>{"Next"}</Button>
+			</Box>
+		</div>
+	);
+};
+const StepFive = ({
+	handleNext,
+	handleBack,
+	outputText,
+	setOutputText,
+}: {
+	handleNext: any;
+	handleBack: any;
+	outputText: string;
+	setOutputText: any;
+}) => {
+	const onSubmit = () => {
+		console.log(JSON.stringify({ outputText }, null, 2));
+		handleNext({ outputText });
+	};
+	return (
+		<div>
+			<div className='step-one'>
+				<h2>Step 5: Review Materials</h2>
+				{outputText}
+				<div>
+					Review the generated materials, make any desired edits, then either download as PDF/docx
+					or continue to step 5: generate materials
+				</div>
+			</div>
+			<Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+				<Button color='inherit' onClick={handleBack} sx={{ mr: 1 }}>
+					Back
+				</Button>
+				<Box sx={{ flex: "1 1 auto" }} />
+
+				<Button onClick={onSubmit}>{"Finish"}</Button>
+			</Box>
 		</div>
 	);
 };
 
-
+export { StepOne, StepTwo, StepThree, StepFour, StepFive };
